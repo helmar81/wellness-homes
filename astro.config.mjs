3a2df96fde049fb5779b6a55d0b6d142a73013a2
helmar81 <helmar81@gmail.com>
2025-07-13 11:08:06 +0200

    Replace project with updated version

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import netlify from '@astrojs/netlify';
+
+// https://astro.build/config
+export default defineConfig({
+  adapter: netlify()
+});
